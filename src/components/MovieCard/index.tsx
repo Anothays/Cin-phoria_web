2617ada1fe116a394528a68678526615518c9576
Anothays <jeremy.snnk@gmail.com>
2024-09-20 11:39:09 +0200

    add MovieCard component

---
+++ src/components/MovieCard/index.tsx
@@ -0,0 +1,28 @@
+import { MovieType } from '@/types/MovieType';
+import Image from 'next/image';
+import Link from 'next/link';
+import styles from './MovieCard.module.scss';
+
+export default function MovieCard(movie: MovieType) {
+  return (
+    <Link href={`/movies/${movie.id}`} className={styles.container}>
+      <Image
+        className={styles.image}
+        // src={movie.img}
+        src={`${process.env.NEXT_PUBLIC_BASE_URL}/uploads/images/${movie.coverImageName}`}
+        // alt={movie.name}
+        alt={`${movie.coverImageName}`}
+        width={290}
+        height={300}
+        sizes="100%"
+        style={{
+          width: '100%',
+          height: 'auto',
+        }}
+      />
+      <div className={styles.titleAndPriceContainer}>
+        <h3 className={styles.text}>{movie.title}</h3>
+      </div>
+    </Link>
+  );
+}
