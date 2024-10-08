76013a925944bce7f2c0d830ba4801073e1a0f43
Anothays <jeremy.snnk@gmail.com>
2024-10-08 11:56:20 +0200

    add lib.ts file

---
+++ src/lib/dal.ts
@@ -0,0 +1,28 @@
+import 'server-only';
+
+import jwt from 'jsonwebtoken';
+import { cookies } from 'next/headers';
+import { readFileSync } from 'node:fs';
+import path from 'node:path';
+
+export const verifySession = () => {
+
+  if (!cookies().has('jwt')) return false
+  const token = cookies().get('jwt');
+  if (!token) return false;
+
+  const publicKeyPath = path.join(process.cwd(), process.env.JWT_PUBLIC_KEY!);
+  const publicKey = readFileSync(publicKeyPath, 'utf8');
+  // console.log(publicKey);
+
+  try {
+    const decoded = jwt.verify(token.value, publicKey, { algorithms: ['RS256'] });
+    // console.log(decoded);
+    if (!decoded) return false;
+    return true;
+
+  } catch (error) {
+    console.error("Erreur de vérification du token : ", error);
+    return false;
+  }
+}
