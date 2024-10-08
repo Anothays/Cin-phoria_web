e3d43cd953553865f1779367685a5771cfd0d884
Anothays <jeremy.snnk@gmail.com>
2024-10-08 11:49:51 +0200

    update globalContext

--- src/context/globalContext.tsx
+++ src/context/globalContext.tsx
@@ -1,23 +1,37 @@
 'use client';
 import { createContext, ReactNode, useContext, useState } from 'react';
 
+type loginFormProps = {
+  title: string;
+  message: string;
+  redirectionUrl: string;
+  callbackAction: (...args: any[]) => any;
+};
+
 type GlobalContextType = {
   isLoginModalOpen: boolean;
   openLoginModal: () => void;
   closeLoginModal: () => void;
   toggleLoginModal: () => void;
-  isLogged: boolean;
-  setIsLogged: (bool: boolean) => void;
+  loginFormProps: loginFormProps;
+  updateLoginProps: (props: Partial<loginFormProps>) => void;
 };
 export const globalContext = createContext<GlobalContextType | undefined>(undefined);
 
 export const GlobalContextHandler = ({ children }: { children: ReactNode }) => {
   const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
-  const [isLogged, setIsLogged] = useState(false);
+  const [loginFormProps, setLoginFormProps] = useState({
+    title: 'Connexion',
+    message: 'Connectez-vous pour accéder à votre compte',
+    redirectionUrl: '/',
+    callbackAction: () => {},
+  });
 
   const openLoginModal = () => setIsLoginModalOpen(true);
   const closeLoginModal = () => setIsLoginModalOpen(false);
   const toggleLoginModal = () => setIsLoginModalOpen((prev) => !prev);
+  const updateLoginProps = (props: Partial<loginFormProps>) =>
+    setLoginFormProps((prev) => ({ ...prev, ...props }));
 
   return (
     <globalContext.Provider
@@ -26,8 +40,8 @@ export const GlobalContextHandler = ({ children }: { children: ReactNode }) => {
         openLoginModal,
         closeLoginModal,
         toggleLoginModal,
-        isLogged,
-        setIsLogged,
+        loginFormProps,
+        updateLoginProps,
       }}
     >
       {children}
