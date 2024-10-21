c187b9a30dccfe83f06211840ad9ce39c75d68c7
Anothays <jeremy.snnk@gmail.com>
2024-10-21 16:20:24 +0200

    fix bug in src/app/movies/[id]/page.tsx

--- src/app/movies/[id]/page.tsx
+++ src/app/movies/[id]/page.tsx
@@ -1,11 +1,11 @@
 import MovieDetails from '@/containers/MovieDetailsPage/MovieDetails';
 import ProjectionEventsTab from '@/containers/MovieDetailsPage/ProjectionEventsTab';
 import fetcher from '@/services/fetcher';
-import { MovieType } from '@/types/MovieType';
 import styles from './MovieDetails.module.scss';
 
 export default async function MovieDetail({ params }: { params: { id: string } }) {
-  const movie = (await fetcher(`/api/movies/${params.id}`)) as MovieType;
+  const response = await fetcher(`/api/movies/${params.id}`);
+  const movie = await response.json();
   const projectionEvents = movie.projectionEvents;
 
   return (
