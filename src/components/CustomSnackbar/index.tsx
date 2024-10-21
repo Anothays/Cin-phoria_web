77375c7654144602dbb43336bbda6a4cb815e53d
Anothays <jeremy.snnk@gmail.com>
2024-10-21 16:32:09 +0200

    add CustomSnackbar component

---
+++ src/components/CustomSnackbar/index.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import { useGlobalContext } from '@/context/globalContext';
+import Alert from '@mui/material/Alert';
+import Snackbar, { SnackbarCloseReason } from '@mui/material/Snackbar';
+
+export default function CustomSnackbar() {
+  const { isSnackbarVisible, closeSnackbar, snackbarContent, setSnackbarContent } =
+    useGlobalContext();
+
+  const handleClose = (event?: React.SyntheticEvent | Event, reason?: SnackbarCloseReason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    closeSnackbar();
+    setSnackbarContent(null);
+  };
+
+  return (
+    <div>
+      <Snackbar open={isSnackbarVisible} autoHideDuration={6000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="success" variant="filled" sx={{ width: '100%' }}>
+          {snackbarContent}
+        </Alert>
+      </Snackbar>
+    </div>
+  );
+}
