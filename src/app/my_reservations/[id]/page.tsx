ff63c99a5603abee738e58547746ef92181ad829
Anothays <jeremy.snnk@gmail.com>
2024-10-21 16:32:37 +0200

    add my_reservations page

---
+++ src/app/my_reservations/[id]/page.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import TicketList from '@/containers/MyReservationPage/TicketList';
+import fetcher from '@/services/fetcher';
+import { ReservationType } from '@/types/ReservationType';
+import { useSession } from 'next-auth/react';
+import { useEffect, useState } from 'react';
+import styles from './Reservation.module.scss';
+
+export default function ReservationPage({ params }: { params: { id: number } }) {
+  const session = useSession();
+  const [reservation, setReservation] = useState<ReservationType | undefined>(undefined);
+
+  useEffect(() => {
+    if (session.status === 'authenticated') {
+      fetcher(`/api/reservations/${params.id}`, {
+        headers: {
+          Authorization: `Bearer ${session.data?.token}`,
+        },
+      }).then((data) => {
+        console.log('reservation ==> ', data);
+        setReservation(data);
+      });
+    }
+  }, [session?.status]);
+
+  if (reservation === undefined) return <p>Chargement...</p>;
+
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Vos billets pour la séance</h1>
+      <TicketList reservation={reservation} />
+    </div>
+  );
+}
---
+++ src/app/my_reservations/page.tsx
@@ -0,0 +1,47 @@
+'use client';
+
+import ReservationCard from '@/components/ReservationCard';
+import fetcher from '@/services/fetcher';
+import { ApiJSONResponseType } from '@/types/ApiResponseType';
+import { ReservationType } from '@/types/ReservationType';
+import { useSession } from 'next-auth/react';
+import { useEffect, useState } from 'react';
+import styles from './MyReservation.module.scss';
+
+export default function MyReservationsPage() {
+  const session = useSession();
+  const [reservations, setReservations] = useState<ReservationType[] | undefined>(undefined);
+
+  useEffect(() => {
+    if (session.status === 'authenticated') {
+      fetcher('/api/reservations', {
+        headers: {
+          Authorization: `Bearer ${session.data?.token}`,
+        },
+      })
+        .then((res) => res.json())
+        .then((res: ApiJSONResponseType) =>
+          setReservations(res['hydra:member'] as ReservationType[]),
+        );
+    }
+  }, [session.status]);
+
+  if (session.status === 'loading') return <p>Chargement...</p>;
+
+  if (session.status === 'authenticated' && reservations) {
+    return (
+      <div className={styles.container}>
+        <h1 className={styles.title}>Mes réservations</h1>
+        <div>
+          {reservations?.length > 0 ? (
+            reservations?.map((reservation) => (
+              <ReservationCard reservation={reservation} key={reservation.id} />
+            ))
+          ) : (
+            <p>Pas de réservation actuellement</p>
+          )}
+        </div>
+      </div>
+    );
+  }
+}
