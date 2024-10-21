d7e8f8b5de9273f29e235858093ba6d50f59fc93
Anothays <jeremy.snnk@gmail.com>
2024-10-21 16:09:55 +0200

    update middle.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,11 +2,15 @@ import { withAuth } from 'next-auth/middleware';
 
 export default withAuth({
   pages: {
-    signIn: '/', // Redirige vers la page d'accueil si non authentifié
+    signIn: '/login',
   },
 });
 
-// Définissez les routes protégées
+
 export const config = {
-  matcher: ['/reservations/:path*'], // Protéger toutes les routes sous /reservations
+  matcher: [
+    '/reservations/:path*',
+    '/my_reservations'
+  ],
+
 };
