403d2b22796a6a9d049502708469505c3e7d7d57
Anothays <jeremy.snnk@gmail.com>
2024-10-21 16:09:12 +0200

    change fetcher

--- src/services/fetcher.ts
+++ src/services/fetcher.ts
@@ -1,7 +1,13 @@
 
 
-const BASE_URL = `${process.env.NEXT_PUBLIC_BASE_URL}`
+const BASE_URL = `${process.env.NEXT_PUBLIC_BASE_URL}`;
+// const session = await getSession();
+// const initDefault: RequestInit = {
+//   headers: {
+//     'Authorization': session?.token ? `Bearer ${session.token}` : '',
+//   }
+// };
 
-const fetcher = (input: string | URL | globalThis.Request, init?: RequestInit, baseUrl: string = BASE_URL) => fetch(`${baseUrl}${input}`, init).then(res => res.json());
+const fetcher = (input: string | URL | globalThis.Request, init?: RequestInit, baseUrl: string = BASE_URL) => fetch(`${baseUrl}${input}`, init);
 
 export default fetcher;
