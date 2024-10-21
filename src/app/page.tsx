5ef09fdbe738f4136ff37898d2c79d4a8cd74021
Anothays <jeremy.snnk@gmail.com>
2024-10-21 16:17:05 +0200

    update src/app/page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,24 @@
 import Hero from '@/containers/HomePage/Hero';
 import MovieList from '@/containers/HomePage/MovieList';
+import getMovies from '@/services/movies/lib';
+import { ApiJSONResponseType } from '@/types/ApiResponseType';
+import { MovieType } from '@/types/MovieType';
+import styles from './HomePage.module.scss';
 
-export default function Home() {
+export default async function Home() {
+  const response = await getMovies('isStaffFavorite=true');
+  const moviesData: ApiJSONResponseType = await response.json();
+  console.log('moviesData', moviesData);
+
+  const movies = moviesData['hydra:member'] as MovieType[];
+
   return (
-    <div>
+    <div className={styles.page}>
       <Hero />
-      <MovieList />
+      <h2 className={styles.title}>Notre sélection</h2>
+      <section className={styles.movieList}>
+        <MovieList movies={movies} />
+      </section>
     </div>
   );
 }
