6510d2b86b7f591f9fb3ec7066d31ef12b606e25
Anothays <jeremy.snnk@gmail.com>
2024-11-02 23:58:20 +0100

    update submit button style/logic into src/app/reservations/[id]/Reservation.module.scss

--- src/app/reservations/[id]/page.tsx
+++ src/app/reservations/[id]/page.tsx
@@ -4,8 +4,8 @@ import SeatMap from '@/components/SeatMap';
 import fetcher from '@/services/fetcher';
 import { ProjectionEventType } from '@/types/ProjectionEventType';
 import { ReservationType } from '@/types/ReservationType';
+import { Button } from '@mui/material';
 import { useSession } from 'next-auth/react';
-import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import styles from './Reservation.module.scss';
@@ -14,6 +14,7 @@ export default function ReservationPage({ params }: { params: { id: number } }) 
   const { status, data } = useSession();
   const router = useRouter();
   const [selectedSeats, setSelectedSeats] = useState<number[]>([]);
+  const [buttonDisabled, setButtonDisabled] = useState(true);
   const [currentReservation, setCurrentReservation] = useState<ReservationType | undefined>(
     undefined,
   );
@@ -32,9 +33,13 @@ export default function ReservationPage({ params }: { params: { id: number } }) 
 
   const handleClick = async (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
     e.preventDefault();
+
+    if (selectedSeats.length <= 0) return alert('Vous devez choisir au moins une place');
+
     const body = {
       seats: selectedSeats.map((seatId) => `/api/projection_room_seats/${seatId}`),
     };
+
     const response = await fetcher(`/api/reservations/${currentReservation?.id}`, {
       method: 'PATCH',
       body: JSON.stringify(body),
@@ -44,6 +49,13 @@ export default function ReservationPage({ params }: { params: { id: number } }) 
       },
     });
     console.log('response ==> ', response);
+    if (!response.projectionEvent) {
+      alert(
+        'Vous avez dépassé les 5 minutes. Votre réservations a été annulée. Veuillez recommencer',
+      );
+      router.replace(`/`);
+      return;
+    }
     router.push(`/reservations/${params.id}/ticket_choice`);
   };
 
@@ -70,6 +82,14 @@ export default function ReservationPage({ params }: { params: { id: number } }) 
     }
   }, [currentReservation?.projectionEvent]);
 
+  useEffect(() => {
+    if (selectedSeats.length > 0) {
+      setButtonDisabled(false);
+    } else {
+      setButtonDisabled(true);
+    }
+  }, [selectedSeats]);
+
   return (
     <div className={styles.container}>
       <h1>
@@ -86,9 +106,14 @@ export default function ReservationPage({ params }: { params: { id: number } }) 
         <SeatMap projectionId={currentProjection?.id} onSeatSelect={handleSeatSelect} />
       ) : null}
 
-      <Link className={styles.seatingConfirmationSection} href={''} onClick={handleClick}>
+      <Button
+        disabled={buttonDisabled}
+        className={buttonDisabled ? styles.submitButtonDisabled : styles.submitButtonEnabled}
+        href={''}
+        onClick={handleClick}
+      >
         Réserver ma place
-      </Link>
+      </Button>
     </div>
   );
 }
