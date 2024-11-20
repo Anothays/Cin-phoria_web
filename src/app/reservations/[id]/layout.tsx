8c0d710ecced9a48b7ae58b47918d3322ea26f50
Anothays <jeremy.snnk@gmail.com>
2024-11-21 00:12:40 +0100

    Add message to notify user of 5-minute window to finalize reservation

--- src/app/reservations/[id]/layout.tsx
+++ src/app/reservations/[id]/layout.tsx
@@ -3,5 +3,12 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <div>{children}</div>;
+  return (
+    <div>
+      {children}
+      <p style={{ paddingTop: '1rem', textAlign: 'center' }}>
+        Passé 5 minutes, vous devrez recommencer votre réservation
+      </p>
+    </div>
+  );
 }
