2273aa0e8f5de303c112bb87616ada2736342ac1
Anothays <jeremy.snnk@gmail.com>
2025-03-21 09:42:12 +0100

    get image only from api hostname

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: '**',
+        hostname: 'cinephoria.jeremysnnk.ovh',
       },
       {
         protocol: 'http',
