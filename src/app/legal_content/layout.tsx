4378c16af35d993233cbdc243421537b62af3512
Anothays <jeremy.snnk@gmail.com>
2025-03-23 15:34:22 +0100

    change text content

--- src/app/legal_content/layout.tsx
+++ src/app/legal_content/layout.tsx
@@ -10,9 +10,7 @@ export default function layout({ children }: { children: React.ReactNode }) {
           fin de formation initiée par l&apos;organisme <a href="https://www.studi.com/fr">STUDI</a>
           . En conséquence, toutes les données apparaissant dans le présent texte, et sur le site
           dans son entièreté, ne sont reliées d&apos;aucune manière à une personne physique ou
-          morale existante. Toutes les images utilisées pour l&apos;illustration du présent site web
-          sont fournies par le site Pixabay. Ces images sont sous licence &quot;Creative Commons
-          Zero (CC0)&quot;. sous licence &quot;Creative Commons Zero (CC0)&quot;.
+          morale existante.
         </p>
       </section>
       {children}
